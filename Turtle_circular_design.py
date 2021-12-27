29b3dcef2b8c4ab6156a94e091d09b589f5f5c77
Swarnima-Gupta <60737601+Swarnima-Gupta@users.noreply.github.com>
2021-12-27 20:37:07 +0530

    Add files via upload

---
+++ Turtle_circular_design.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 27 20:35:15 2021
+
+@author: Rajeev Gupta
+"""
+
+import turtle
+import colorsys
+
+t = turtle.Turtle()
+s = turtle.Screen()
+s.bgcolor('black')
+t.speed(0)
+n = 36
+h = 0
+for i in range(360):
+    c = colorsys.hsv_to_rgb(h, 1, 0.8)
+    h+= 1/n
+    t.color(c)
+    t.circle(180)
+    t.left(10)
